067aaf876b8dfe0e3ed46876199b92b20eb6c31c
Criss Guinther <crissrobertg@gmail.com>
2022-03-26 19:56:26 -0300

    Spring and templates

---
+++ src/main/java/com/crissguinther/AlgaWorksContatoApplication.java
@@ -0,0 +1,13 @@
+package com.crissguinther;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaWorksContatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgaWorksContatoApplication.class, args);
+	}
+
+}
