cae1b9dc85fa4dae2110dca89e3181397ac3f239
crissguinther <crissrobertg@gmail.com>
2022-03-30 17:44:42 -0300

    Metodo remover adicionado

--- src/main/java/com/crissguinther/ContatosControle.java
+++ src/main/java/com/crissguinther/ContatosControle.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -70,6 +71,20 @@ public class ContatosControle {
 		contato.setId(id);
 
 		LISTA_CONTATOS.add(contato);
+
+		return "redirect:/contatos";
+	}
+
+	@DeleteMapping("/contatos/{id}")
+	public String deletar(@PathVariable String id) {
+
+		for (Contato contato : LISTA_CONTATOS) {
+			if (contato.getId().equals(id)) {
+				LISTA_CONTATOS.remove(contato);
+				break;
+			} else
+				continue;
+		}
 
 		return "redirect:/contatos";
 	}
